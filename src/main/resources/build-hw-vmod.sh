0c53242288edbc63ba61afa5799ae1862842558f
Tynan McAuley <tynan@galois.com>
2020-05-26 15:07:32 -0700

    Refactor NVDLA RTL build script
    This makes it a bit easier to add new NVDLA configurations.

--- src/main/resources/build-hw-vmod.sh
+++ src/main/resources/build-hw-vmod.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-# script to build both the small and large versions of nvdla
+# Build RTL for multiple NVDLA configurations
 
 set -ex
 
-cp tree.make hw
+cd "$(dirname "$0")"
+
+function build_nvdla_rtl() {
+    local NVDLA_TYPE="$1"
+    echo "Building NVDLA configuration: \"$NVDLA_TYPE\""
+    cp tree.make hw
+    sed -i -e "s/nv_small/nv_$NVDLA_TYPE/" hw/tree.make
+    cd hw
+    ./tools/bin/tmake -build vmod
+    cd ..
+}
+
+function install_nvdla_rtl() {
+    local NVDLA_TYPE="$1"
+    local SRC_DIR=hw/outdir/nv_$NVDLA_TYPE/vmod
+    local DST_DIR=vsrc/$NVDLA_TYPE
+    rm -rf "$DST_DIR/vmod"
+    mkdir -p "$DST_DIR"
+    cp -r "$SRC_DIR" "$DST_DIR"
+}
 
 NVDLA_TYPE=small
+build_nvdla_rtl $NVDLA_TYPE
+install_nvdla_rtl $NVDLA_TYPE
 
-echo "Build NVDLA \"$NVDLA_TYPE\""
-cd hw
-./tools/bin/tmake -build vmod
-cd ..
-rm -rf vsrc/$NVDLA_TYPE/vmod
-mkdir -p vsrc/$NVDLA_TYPE
-cp -r hw/outdir/nv_$NVDLA_TYPE/vmod vsrc/$NVDLA_TYPE
-
-sed -i -e 's/nv_small/nv_large/' hw/tree.make
 NVDLA_TYPE=large
-
-echo "Build NVDLA \"$NVDLA_TYPE\""
-cd hw
-./tools/bin/tmake -build vmod
-cd ..
-rm -rf vsrc/$NVDLA_TYPE/vmod
-mkdir -p vsrc/$NVDLA_TYPE
-cp -r hw/outdir/nv_$NVDLA_TYPE/vmod vsrc/$NVDLA_TYPE
+build_nvdla_rtl $NVDLA_TYPE
+install_nvdla_rtl $NVDLA_TYPE
